cee7a79209985d63ca6923bb77fde41f26f77159
TsenHill <chou870808@gmail.com>
2021-11-16 14:44:29 +0800

    first commit

---
+++ FunctionLibary.h
@@ -0,0 +1,14 @@
+#ifndef TASK_H
+#define TASK_H
+
+#include "scheduling_simulator.h"
+
+void task1(void);
+void task2(void);
+void task3(void);
+void task4(void);
+void task5(void);
+void task6(void);
+void task7(ProcessData *Parent);
+void task8(ProcessData *Parent);
+#endif
