804cec09cbcba6c824bd9e475467b893bbee6134
TsenHill <chou870808@gmail.com>
2021-11-21 16:34:21 +0800

    Remove 2017api

--- function_libary.c
+++ function_libary.c
@@ -25,7 +25,6 @@ void function2(void)
 void function3(void)
 {
 	// wait infinite
-	hw_suspend(32768);
 	fprintf(stdout, "task3: good morning~\n");
 	fflush(stdout);
 }
@@ -33,36 +32,24 @@ void function3(void)
 void function4(void)
 {
 	// sleep 5s
-	hw_suspend(500);
 	fprintf(stdout, "task4: good morning~\n");
 	fflush(stdout);
 }
 
 void function5(void)
 {
-	int pid = hw_task_create("task3");
 
-	hw_suspend(1000);
 	fprintf(stdout, "task5: good morning~\n");
 	fflush(stdout);
 
-	hw_wakeup_pid(pid);
-	fprintf(stdout, "Mom(task5): wake up pid %d~\n", pid);
-	fflush(stdout);
 }
 
 void function6(void)
 {
-	for (int num = 0; num < 5; ++num) 
-	{
-		hw_task_create("task3");
-	}
 
-	hw_suspend(1000);
+
 	fprintf(stdout, "task6: good morning~\n");
 	fflush(stdout);
 
-	int num_wake_up = hw_wakeup_taskname("task3");
-	fprintf(stdout, "Mom(task6): wake up task3=%d~\n", num_wake_up);
-	fflush(stdout);
+
 }
