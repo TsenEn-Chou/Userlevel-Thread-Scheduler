bd9f510c3e4a721de93abc13b7e8f152a168433a
TsenHill <chou870808@gmail.com>
2021-11-21 13:21:07 +0800

    Follow google c++ style

---
+++ function_libary.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTION_LIBARY_H
+#define FUNCTION_LIBARY_H
+
+#include "os2021_api.h"
+
+void function1(void);
+void function2(void);
+void function3(void);
+void function4(void);
+void function5(void);
+void function6(void);
+#endif
