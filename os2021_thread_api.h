bf041525dba90c24e83b12d841215e5374b59400
TsenHill <chou870808@gmail.com>
2021-11-22 13:52:04 +0800

    Implement api

--- os2021_thread_api.h
+++ os2021_thread_api.h
@@ -9,17 +9,15 @@
 void CreateContext(ucontext_t *, ucontext_t *, void *);
 void InitAllQueues();
 int CheckBitMap(list_t *queue);
+int CheckQueueHaveNode(list_t *queue, int priority);
+int AssignTQ(TCB *node);
+void RunTask();
 
 int OS2021_ThreadCreate(char *job_name, char *p_function, int priority, int cancel_mode);
 void OS2021_ThreadCancel(char *job_name);
-
 void OS2021_ThreadWaitEvent(int event_id);
 void OS2021_ThreadSetEvent(int event_id);
-void OS2021_ThreadSuspend(int msec);
-
-
-
-void RunTask();
+void OS2021_ThreadWaitTime(int msec);
 
 void TimerCalc();
 void Dispatcher();
