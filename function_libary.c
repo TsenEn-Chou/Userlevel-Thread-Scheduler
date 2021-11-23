357be717d8dc412f05f2d615da92e0cb0c63057c
TsenEN <chou870808@gmail.com>
2021-11-24 01:37:14 +0800

    New function6 is use by idle task

--- function_libary.c
+++ function_libary.c
@@ -15,10 +15,8 @@ void function2(void)
 {
 	// run infinite
 	unsigned int a = 0;
-
-	while (1) 
-	{
-		a = a + 1;
+	while (1) {
+		a++;
 	}
 }
 
@@ -27,7 +25,7 @@ void function3(void)
 	
 	fprintf(stdout, "task3: good morning~\n");
 	fflush(stdout);
-	OS2021_ThreadWaitTime(100);
+	OS2021_ThreadWaitTime(1000);
 	fprintf(stdout, "task3: good morning again~\n");
 	fflush(stdout);
 }
@@ -37,7 +35,7 @@ void function4(void)
 	// sleep 5s
 	fprintf(stdout, "task4: good morning~\n");
 	fflush(stdout);
-	OS2021_ThreadWaitEvent(1);
+	//OS2021_ThreadWaitEvent(1);
 	fprintf(stdout, "task4: wake up by task5~\n");
 	fflush(stdout);
 }
@@ -47,18 +45,16 @@ void function5(void)
 
 	fprintf(stdout, "task5: good morning~\n");
 	fflush(stdout);
-	OS2021_ThreadSetEvent(1);
-	fprintf(stdout, "task4: wake up task4~\n");
+	//OS2021_ThreadSetEvent(1);
+	fprintf(stdout, "task5: wake up task4~\n");
 	fflush(stdout);
 
 }
 
 void function6(void)
 {
-
-
-	fprintf(stdout, "task6: good morning~\n");
-	fflush(stdout);
-
-
+	// run infinite
+	while (1) {
+		CheckTerminateQueue();
+	}
 }
